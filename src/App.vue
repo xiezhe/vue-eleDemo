10cf72a7ede8e43466808bba6eeef0ea2456691d
xiezhe <xiezhev@gmail.com>
2017-03-16 00:58:56 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/header/header';
+const ERR_OK = 0;
+export default {
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  components: {
+    'v-header': Header
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      if (response.data.errno === ERR_OK) {
+        this.seller = response.data.data;
+      }
+    });
+  },
+  name: 'app'
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="header">
+		<div class="content-wrapper">
+			<div class="avatar">
+				<img width="64" height="64" :src="seller.avatar"></img>
+			</div>
+			<div class="content">
+				<div class="title">
+					<span class="brand"></span>
+					<span class="name">{{seller.name}}</span>
+				</div>
+				<div class="description">
+					{{seller.description}}/{{seller.deliveryTime}}分钟送达
+				</div>
+				<div v-if="seller.supports" class="support">
+					<span class="icon"></span>
+					<span>{{seller.supports[0].description}}</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
+</script>
+<style>
+	.header{
+		background:  #fff;
+		background-color: #000;
+	}
+	.content-wrapper{
+		padding:  24px 12px 18px 24px;
+	}
+	.avatar{
+		display: inline-block;
+	}
+	.content{
+		display: inline-block;
+	}
+</style>
